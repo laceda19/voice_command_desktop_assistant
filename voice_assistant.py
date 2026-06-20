72252b0d2bfa2135cc284a77e293e727cd6331f8
laceda19 <gelailaceda@gmail.com>
2026-06-20 09:44:48 -0700

    Add speech recognition functionality

--- voice_assistant.py
+++ voice_assistant.py
@@ -7,3 +7,23 @@ class VoiceAssistant(Assistant):
         self.__version = "1.0"
 
         self.recognizer = sr.Recognizer()
+
+    def listen(self):
+
+        with sr.Microphone() as source:
+
+            print("\n🎤 Listening...")
+
+            self.recognizer.adjust_for_ambient_noise(source, duration=0.3)
+
+            try:
+                audio = self.recognizer.listen(source, timeout=5)
+
+                command = self.recognizer.recognize_google(audio).lower()
+
+                print(f"✅ YOU SAID: {command}")
+
+                return command
+
+            except:
+                return ""
